5f11d5e091848e40a5825f13cdccf02a8569b4b8
Tim Buckley <timothy.jas.buckley@hpe.com>
2015-11-24 13:48:47 -0700

    Removed unused node dependencies.
    This removes `gulp-imagemin` and the image processing gulp task,
    as well as `debowerify`, significantly reducing required
    `npm install` time for a clean install.
    
    Change-Id: I48e035c96cd616715da3b5c75b66b8d8a7616263

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,7 +7,6 @@ gulp.task('watch', ['browserSync', 'server'], function() {
 
   // Scripts are automatically watched and rebundled by Watchify inside Browserify task
   gulp.watch(config.styles.src,  ['styles']);
-  gulp.watch(config.images.src,  ['images']);
   gulp.watch(config.fonts.src,   ['fonts']);
   gulp.watch(config.data.src,    ['data']);
   gulp.watch(config.views.watch, ['views']);
