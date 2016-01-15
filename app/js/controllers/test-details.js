6b80ea8fa2747e2bb0cb7948e5e522ab1de2c810
Austin Clark <austin.clark@hpe.com>
2016-01-15 13:20:21 -0700

    Add filter functionality to test-details pythonlogging
    On tests with pythonlogging, the DEBUG headers can be quite long and
    make finding useful information difficult. This patch adds a small,
    four-button filter to the test-details page that allows a user to
    select which levels of logging to be displayed (INFO, WARNING, ERROR,
    DEBUG). This patch will likely serve as the base for additional search
    functionality for test details (e.g. queries).
    
    Change-Id: Ia4286861268de0e43313d3b8d7f966e94697a495

--- app/js/controllers/test-details.js
+++ app/js/controllers/test-details.js
@@ -8,14 +8,14 @@ var controllersModule = require('./_index');
 function TestDetailsCtrl($scope, $location, $stateParams, datasetService) {
    // ViewModel
   var vm = this;
-  vm.datasetId = $stateParams.datasetId;
+  $scope.datasetId = $stateParams.datasetId;
   var testName = $stateParams.test;
-  vm.testName = testName;
+  $scope.testName = testName;
 
   // load dataset, raw json, and details json
   datasetService.get($stateParams.datasetId).then(function(response) {
-    vm.dataset = response;
-    vm.stats = response.stats;
+    $scope.dataset = response;
+    $scope.stats = response.stats;
     datasetService.raw(response).then(function(raw) {
       var item = null;
       for (var t in raw.data) {
@@ -23,19 +23,44 @@ function TestDetailsCtrl($scope, $location, $stateParams, datasetService) {
           item = raw.data[t];
         }
       }
-      vm.item = item;
+      $scope.item = item;
     }).catch(function(ex) {
       $log.error(ex);
     });
     datasetService.details(response).then(function(deets) {
-      vm.details = deets;
-      vm.itemDetails = deets.data[testName];
+      $scope.details = deets;
+      $scope.originalDetails = angular.copy(deets.data[testName]);
+      $scope.itemDetails = deets.data[testName];
     }).catch(function(ex) {
       $log.error(ex);
     });
   }).catch(function(ex) {
     $log.error(ex);
   });
+
+  $scope.parsePythonLogging = function(showINFO, showDEBUG, showWARNING, showERROR) {
+    if ($scope.originalDetails && $scope.originalDetails.pythonlogging) {
+      var log = $scope.originalDetails.pythonlogging;
+      var ret = [];
+      var lines = log.split('\n');
+      for (var i in lines) {
+        var line = lines[i];
+        if (showINFO && line.includes("INFO")) {
+          ret.push(line);
+        }
+        else if (showDEBUG && line.includes("DEBUG")) {
+          ret.push(line);
+        }
+        else if (showWARNING && line.includes("WARNING")) {
+          ret.push(line);
+        }
+        else if (showERROR && line.includes("ERROR")) {
+          ret.push(line);
+        }
+      }
+      $scope.itemDetails.pythonlogging = ret.join('\n');
+    }
+  };
 
 }
 controllersModule.controller('TestDetailsCtrl', TestDetailsCtrl);
---
+++ app/js/directives/test-details-search.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var directivesModule = require('./_index.js');
+
+/**
+ * @ngInject
+ */
+function testDetailsSearch() {
+
+  /**
+   * @ngInject
+   */
+  var controller = function($scope, $element) {
+    var self = this;
+    this.open = false;
+    this.showINFO = true;
+    this.showDEBUG = true;
+    this.showWARNING = true;
+    this.showERROR = true;
+
+    var update = function() {
+      $scope.$parent
+            .parsePythonLogging(self.showINFO, self.showDEBUG, self.showWARNING, self.showERROR);
+    };
+
+    $scope.$watch(function() { return self.query; }, update);
+    $scope.$watch(function() { return self.showINFO; }, update);
+    $scope.$watch(function() { return self.showDEBUG; }, update);
+    $scope.$watch(function() { return self.showWARNING; }, update);
+    $scope.$watch(function() { return self.showERROR; }, update);
+  };
+
+  return {
+    restrict: 'EA',
+    require: ['^testDetailsSearch', '^testDetails'],
+    scope: {
+      'parsePythonLogging': '&'
+    },
+    controller: controller,
+    controllerAs: 'search',
+    templateUrl: 'directives/test-details-search.html',
+    transclude: true
+  };
+}
+
+directivesModule.directive('testDetailsSearch', testDetailsSearch);
