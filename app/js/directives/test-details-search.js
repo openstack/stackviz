e3af3b2bd377134eb5405980378f8b0ca087fb1c
Austin Clark <austin.clark@hpe.com>
2016-03-24 13:32:05 -0600

    Add to stackviz-export and stackviz-front docs
    Adds additional information to the stackviz-export and stackviz-front
    technical documents. Info about `stackviz-export` output files is
    now included in the corresponding document. Directive and service
    descriptions were added to stackviz-front, alongside inline comments
    in the appropriate .js files.
    
    Due to the unique structure of GitHub's RST renderer, includes will
    no longer be used in the main README.
    
    Change-Id: Iaebf1f1c3b5e4cbb4ea5e262f85672bc082bbe2f

--- app/js/directives/test-details-search.js
+++ app/js/directives/test-details-search.js
@@ -10,7 +10,14 @@ function testDetailsSearch() {
   /**
    * @ngInject
    */
-  var controller = function($scope, $element) {
+  var controller =
+  /**
+   * Responsible for calling the `parsePythonLogging` filter function in
+   * `TestDetailsController` when the log level buttons change state. The
+   * `filter` function is passed from `test-details` to `test-details-search`
+   * when the directive is initially instantiated.
+   */
+  function($scope, $element) {
     var self = this;
     this.open = false;
     this.showINFO = true;
@@ -18,10 +25,12 @@ function testDetailsSearch() {
     this.showWARNING = true;
     this.showERROR = true;
 
+    // Wrapper for parent controller's filter function.
     var update = function() {
       $scope.filter(self.showINFO, self.showDEBUG, self.showWARNING, self.showERROR);
     };
 
+    // Watchers to signal update function upon button state change.
     $scope.$watch(function() { return self.query; }, update);
     $scope.$watch(function() { return self.showINFO; }, update);
     $scope.$watch(function() { return self.showDEBUG; }, update);
